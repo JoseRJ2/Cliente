ebe00c461447d5816682b1d800e2bbb21aa1a992
JoseRJ2 <147292005+JoseRJ2@users.noreply.github.com>
2024-02-11 22:22:56 -0600

    actualizacion sin catalogo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,12 +7,17 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   Rol: number = 0;
-  constructor() {
+  constructor(private router: Router) {
     const storedRol = localStorage.getItem('Rol');
     
     if (storedRol !== null && !isNaN(parseInt(storedRol))) {
       this.Rol = parseInt(storedRol);
       console.log(this.Rol);
     }
+  }
+  logout() {
+    localStorage.removeItem('Rol');
+    this.Rol = 0;
+    this.router.navigate(['']);
   }
 }
--- src/app/components/productos-cliente/productos-cliente.component.ts
+++ src/app/components/productos-cliente/productos-cliente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from './../../models/productos';
 import { ProductosService } from './../../services/productos.service';
+import e from 'cors';
 declare var $: any;
 @Component({
   selector: 'app-productos-cliente',
@@ -11,6 +12,7 @@ export class ProductosClienteComponent implements OnInit{
   constructor(private productosService: ProductosService){
   }
   productos: Producto[] = [];
+  producto: Producto = new Producto();
   ngOnInit(): void {
     $(document).ready(function(){
       $('.dropdown-trigger').dropdown();
@@ -23,4 +25,12 @@ export class ProductosClienteComponent implements OnInit{
       this.productos = data;
     });
   }
+
+  buscarProducto(producto: any){
+    this.productos= [];
+    this.productosService.listOne(producto).subscribe((data: any) => {
+      this.productos =[ data ];
+    }, error => console.error(error));
+  }
+
 }
